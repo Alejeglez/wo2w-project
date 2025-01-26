e6895bc80b464e770eab1e2894467d09e5d3b1e4
eduardolfULPGC2003 <eduardolf2003@gmail.com>
2025-01-26 09:53:12 +0000

    Las versiones de la lamda del DataProcessingService y la API del QueryService no eran las correctas

--- DataProcessingService/lambda_function.py
+++ DataProcessingService/lambda_function.py
@@ -12,67 +12,107 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     message = event['Records'][0]['Sns']['Message'] 
-    destination_bucket = "s3-wo2w-results"
+    destination_bucket = "s3-wo2w-data-processing"
     
+    data = ""
+
     try:
         processed_content = []
 
-        print(message)
 
+        # Parsear el mensaje si es string
+        
         if isinstance(message, str):
-            # Si es una cadena, lo cargamos como JSON
             data = json.loads(message)
+
         else:
-            # Si no es una cadena, asumimos que ya es un objeto JSON
             data = message
 
-        G = nx.DiGraph()
+        object_key = data["request_id"]
 
-        for path in data["paths"]:
-            for segment in path:
-                # Peso invertido
-                inverted_weight = 1 / int(segment["averageOccurrences"])
-                
-                G.add_edge(
-                    segment["from"], 
-                    segment["to"], 
-                    weight=inverted_weight,
-                    averageOccurrences=int(segment["averageOccurrences"])
-                )
-
-        key = data['algorithm']    
-
-        if "astart" in key:
-            def heuristic(u, target):
-                return abs(G.nodes[u].get('averageOccurrences', 0) - G.nodes[target].get('averageOccurrences', 0))
-            shortest_path = nx.astar_path(G, source="pero", target="peras", heuristic=heuristic, weight="weight")
-            shortest_path_length = nx.astar_path_length(G, source="pero", target="peras", heuristic=heuristic, weight="weight")
-        elif "dijkstra" in key:
-            shortest_path = nx.dijkstra_path(G, source="pero", target="peras", weight="weight")
-            shortest_path_length = nx.dijkstra_path_length(G, source="pero", target="peras", weight="weight")
-        else:
-            raise ValueError("El archivo no contiene 'astart' o 'dijkstra' en su nombre.")
+        #comprobar si data tiene un campo paths
+        if "paths" in data:
         
-        total_average_occurrences = 0
-        for i in range(len(shortest_path) - 1):
-            u = shortest_path[i]
-            v = shortest_path[i + 1]
-            total_average_occurrences += G[u][v]['averageOccurrences']
+            G = nx.DiGraph()
 
-        data['result'] = {
-            'shortestPath': shortest_path,
-            'shortestPathLength': shortest_path_length,
-            'totalAverageOccurrences': total_average_occurrences
-        }
+            for path in data["paths"]:
+                for segment in path:
+                    # Peso invertido
+                    inverted_weight = 1 / float(segment["averageOccurrences"])
+                    
+                    G.add_edge(
+                        segment["from"], 
+                        segment["to"], 
+                        weight=inverted_weight,
+                        averageOccurrences=float(segment["averageOccurrences"])
+                    )
 
-        # Escribimos el resultado en el bucket de destino
-        cleaned_key = data['from'] + "_" + data['to']
-        result_key = f'{cleaned_key}_result.json'
+            key = data['algorithm']   
+
+            source = data['from']
+            target = data['to'] 
+
+            if "astart" in key:
+                def heuristic(u, target):
+                    return abs(G.nodes[u].get('averageOccurrences', 0) - G.nodes[target].get('averageOccurrences', 0))
+                shortest_path = nx.astar_path(G, source=source, target=target, heuristic=heuristic, weight="weight")
+                shortest_path_length = nx.astar_path_length(G, source=source, target=target, heuristic=heuristic, weight="weight")
+            elif "dijkstra" in key:
+                shortest_path = nx.dijkstra_path(G, source=source, target=target, weight="weight")
+                shortest_path_length = nx.dijkstra_path_length(G, source=source, target=target, weight="weight")
+            else:
+                raise ValueError("El archivo no contiene 'astart' o 'dijkstra' en su nombre.")
+            
+            total_average_occurrences = 0
+            for i in range(len(shortest_path) - 1):
+                u = shortest_path[i]
+                v = shortest_path[i + 1]
+                total_average_occurrences += G[u][v]['averageOccurrences']
+
+            data['result'] = {
+                'shortestPath': shortest_path,
+                'shortestPathLength': shortest_path_length,
+                'totalAverageOccurrences': total_average_occurrences
+            }
+
+
+        
+        else:
+            G = nx.Graph()
+
+            for node in data["nodes"]:
+                node_value = node["value"]
+                connected_nodes = node["connectedNodes"]
+                for connected in connected_nodes:
+                    G.add_edge(node_value, connected)
+
+            connected_components = list(nx.connected_components(G))
+
+            subgraphs = [G.subgraph(component).copy() for component in connected_components]
+
+            list_subgraphs = []
+
+            for i, sg in enumerate(subgraphs):
+                list_subgraph = []
+
+                for node in sg.nodes():
+                    list_subgraph.append(node)
+                
+                list_subgraphs.append(list_subgraph)
+
+            dictionary_nodes = {}
+            dictionary_nodes["subgraphs"] = list_subgraphs
+            cleaned_key = "nodes_in_clusters"
+
+            data = dictionary_nodes
+
+
+        result_key = f'{object_key}.json'
         s3.put_object(Bucket=destination_bucket, Key=result_key, Body=json.dumps(data).encode('utf-8'))
 
         return {
             'statusCode': 200,
-            'body': json.dumps(f'Archivo {key} procesado exitosamente.')
+            'body': json.dumps(f'Archivo {object_key} procesado exitosamente.')
         }
     
     except Exception as e:
--- QueryService/query-service-api.py
+++ QueryService/query-service-api.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 
 PATH_REQUEST_COUNT = Counter('path_requests_total', 'Total de solicitudes recibidas (paths)')
+SHORTEST_PATH_REQUEST_COUNT = Counter('shortest_path_requests_total', 'Total de solicitudes recibidas (shortest path)')
 NODES_WITH_CONNECTIVITY_DEGREE_REQUEST_COUNT = Counter('nodes_with_connectivity_degree_requests_total', 'Total de solicitudes recibidas (nodes with connectivity degree)')
 MAX_DISTANCE_PATH_REQUEST_COUNT = Counter('max_distance_path_requests_total', 'Total de solicitudes recibidas (max distance path)')
 NODES_WITH_HIGHEST_CONNECTIONS_REQUEST_COUNT = Counter('nodes_with_highest_connections_requests_total', 'Total de solicitudes recibidas (nodes with highest connections)')
@@ -22,14 +23,14 @@ REQUEST_IN_PROGRESS = Gauge('query_requests_in_progress', 'Solicitudes en progre
 REQUEST_LATENCY = Histogram('query_request_latency_seconds', 'Distribución de latencias')
 REQUEST_STATS = Summary('query_request_summary_seconds', 'Estadísticas de latencias')
 
-elb_dns = "external-LB-reader-685281967.us-east-1.elb.amazonaws.com"
-sns_arn = "arn:aws:sns:us-east-1:152286733504:prueba"
+elb_dns = "external-lb-768934270.us-east-1.elb.amazonaws.com"
+sns_arn = "arn:aws:sns:us-east-1:152286733504:request-processing-topic"
 s3_bucket = "s3-wo2w-data-processing"
 s3 = boto3.client('s3', region_name='us-east-1')
 sns = boto3.client('sns', region_name='us-east-1')
 
 @app.route('/findPaths', methods=['GET'])
-def factorial():
+def findPaths():
     global REQUEST_ID
     id = "1" + str(REQUEST_ID)
     REQUEST_ID += 1
@@ -46,6 +47,35 @@ def factorial():
         response = requests.get(f'http://{elb_dns}/read?method=findPaths&start={origin}&end={to}')
         response = response.json()
         response['data']['request_id'] = id
+        return response, 200
+    except Exception as e:
+        return {"message": str(e)}, 500
+    finally:
+        latency = time.time() - start_time
+        REQUEST_LATENCY.observe(latency)  # Registrar latencia en histogram
+        REQUEST_STATS.observe(latency)  # Registrar latencia en summary
+        REQUEST_IN_PROGRESS.dec()  # Decrementar gauge de solicitudes en progreso
+
+@app.route('/findShortestPaths', methods=['GET'])
+def findShortestPath():
+    global REQUEST_ID
+    id = "1" + str(REQUEST_ID)
+    REQUEST_ID += 1
+    SHORTEST_PATH_REQUEST_COUNT.inc()
+    REQUEST_IN_PROGRESS.inc()  # Incrementar gauge de solicitudes en progreso
+    
+    
+    start_time = time.time()
+    try:
+        origin = request.args.get('from')
+        to = request.args.get('to')
+        algorithm = request.args.get('method')
+        if not origin or not to:
+            return {400: "Debe especificar un origen y un destino"}, 400
+        response = requests.get(f'http://{elb_dns}/read?method=findPaths&start={origin}&end={to}')
+        response = response.json()
+        response['data']['request_id'] = id
+        response['data']['algorithm'] = algorithm
         sns.publish(TopicArn=sns_arn, Message=str(response["data"]).replace("'", '"'))
         while True:
             try:
@@ -76,7 +106,7 @@ def findNodesWithConnectivityDegree():
         number = request.args.get('number')
         if not number:
             return {400: "Debe especificar un número"}, 400
-        response = requests.get(f'http://{elb_dns}/read?method=findNodesWithConnectivityDegree')
+        response = requests.get(f'http://{elb_dns}/read?method=findNodesWithConnectivityDegree&number={number}')
         return jsonify(response.json()), 200
     except Exception as e:
         return {"message": str(e)}, 500
